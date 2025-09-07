1c888c1c7fcfa859b427ea3d4254cab46d66d02a
Sandesh Sanjeev <sandesh.achr@gmail.com>
2025-09-07 09:53:50 -0700

    Adds PBT for most blanket supported record types

--- crosstream-ring/src/segment.rs
+++ crosstream-ring/src/segment.rs
@@ -365,7 +365,8 @@ mod tests {
     use bolero::{check, generator::*};
     use std::fmt::Debug;
 
-    const SEG_CAPACITY: usize = 1024 * 1024;
+    // Anout 1 MB worth of memory.
+    const SEG_MEMORY: usize = 1024 * 1024;
 
     #[derive(Debug, TypeGenerator)]
     enum Operation<T: TypeGenerator + Debug> {
@@ -382,8 +383,9 @@ mod tests {
                 check!()
                     .with_type::<(Vec<Operation<$num>>, Trimmer)>()
                     .for_each(|(operations, trimmer)| {
-                        let mut mmap = MmapSegment::with_capacity(SEG_CAPACITY, *trimmer);
-                        let mut vec = VecSegment::with_capacity(SEG_CAPACITY, *trimmer);
+                        let capacity = SEG_MEMORY / <$num>::size();
+                        let mut mmap = MmapSegment::with_capacity(capacity, *trimmer);
+                        let mut vec = VecSegment::with_capacity(capacity, *trimmer);
 
                         for operation in operations {
                             match operation {
@@ -428,4 +430,121 @@ mod tests {
     state_machine_test!(state_machine_i64, i64);
     state_machine_test!(state_machine_i128, i128);
     state_machine_test!(state_machine_isize, isize);
+
+    state_machine_test!(state_machine_u8_2, [u8; 2]);
+    state_machine_test!(state_machine_ui_2, [i8; 2]);
+
+    state_machine_test!(state_machine_u8_4, [u8; 4]);
+    state_machine_test!(state_machine_ui_4, [i8; 4]);
+
+    state_machine_test!(state_machine_u8_8, [u8; 8]);
+    state_machine_test!(state_machine_ui_8, [i8; 8]);
+
+    state_machine_test!(state_machine_u8_16, [u8; 16]);
+    state_machine_test!(state_machine_ui_16, [i8; 16]);
+
+    state_machine_test!(state_machine_u8_32, [u8; 32]);
+    state_machine_test!(state_machine_ui_32, [i8; 32]);
+
+    state_machine_test!(state_machine_u8_64, [u8; 64]);
+    state_machine_test!(state_machine_ui_64, [i8; 64]);
+
+    state_machine_test!(state_machine_u8_3, [u8; 3]);
+    state_machine_test!(state_machine_ui_3, [i8; 3]);
+
+    state_machine_test!(state_machine_u8_9, [u8; 9]);
+    state_machine_test!(state_machine_ui_9, [i8; 9]);
+
+    state_machine_test!(state_machine_u8_27, [u8; 27]);
+    state_machine_test!(state_machine_ui_27, [i8; 27]);
+
+    state_machine_test!(state_machine_u8_81, [u8; 81]);
+    state_machine_test!(state_machine_ui_81, [i8; 81]);
+
+    state_machine_test!(state_machine_u8_5, [u8; 5]);
+    state_machine_test!(state_machine_ui_5, [i8; 5]);
+
+    state_machine_test!(state_machine_u8_25, [u8; 25]);
+    state_machine_test!(state_machine_ui_25, [i8; 25]);
+
+    state_machine_test!(state_machine_u8_125, [u8; 125]);
+    state_machine_test!(state_machine_ui_125, [i8; 125]);
+
+    state_machine_test!(state_machine_u16_1, [u16; 1]);
+    state_machine_test!(state_machine_u16_2, [u16; 2]);
+    state_machine_test!(state_machine_u16_3, [u16; 3]);
+    state_machine_test!(state_machine_u16_4, [u16; 4]);
+    state_machine_test!(state_machine_u16_5, [u16; 5]);
+    state_machine_test!(state_machine_u16_6, [u16; 6]);
+    state_machine_test!(state_machine_u16_7, [u16; 7]);
+    state_machine_test!(state_machine_u16_8, [u16; 8]);
+    state_machine_test!(state_machine_u16_19, [u16; 9]);
+    state_machine_test!(state_machine_u16_10, [u16; 10]);
+    state_machine_test!(state_machine_u16_11, [u16; 11]);
+    state_machine_test!(state_machine_u16_12, [u16; 12]);
+    state_machine_test!(state_machine_u16_13, [u16; 13]);
+    state_machine_test!(state_machine_u16_14, [u16; 14]);
+    state_machine_test!(state_machine_u16_15, [u16; 15]);
+    state_machine_test!(state_machine_u16_16, [u16; 16]);
+
+    state_machine_test!(state_machine_u32_1, [u32; 1]);
+    state_machine_test!(state_machine_u32_2, [u32; 2]);
+    state_machine_test!(state_machine_u32_3, [u32; 3]);
+    state_machine_test!(state_machine_u32_4, [u32; 4]);
+    state_machine_test!(state_machine_u32_5, [u32; 5]);
+    state_machine_test!(state_machine_u32_6, [u32; 6]);
+    state_machine_test!(state_machine_u32_7, [u32; 7]);
+    state_machine_test!(state_machine_u32_8, [u32; 8]);
+
+    state_machine_test!(state_machine_u64_1, [u64; 1]);
+    state_machine_test!(state_machine_u64_2, [u64; 2]);
+    state_machine_test!(state_machine_u64_3, [u64; 3]);
+    state_machine_test!(state_machine_u64_4, [u64; 4]);
+
+    state_machine_test!(state_machine_usize_1, [usize; 1]);
+    state_machine_test!(state_machine_usize_2, [usize; 2]);
+    state_machine_test!(state_machine_usize_3, [usize; 3]);
+    state_machine_test!(state_machine_usize_4, [usize; 4]);
+
+    state_machine_test!(state_machine_u128_1, [u128; 1]);
+    state_machine_test!(state_machine_u128_2, [u128; 2]);
+
+    state_machine_test!(state_machine_i16_1, [i16; 1]);
+    state_machine_test!(state_machine_i16_2, [i16; 2]);
+    state_machine_test!(state_machine_i16_3, [i16; 3]);
+    state_machine_test!(state_machine_i16_4, [i16; 4]);
+    state_machine_test!(state_machine_i16_5, [i16; 5]);
+    state_machine_test!(state_machine_i16_6, [i16; 6]);
+    state_machine_test!(state_machine_i16_7, [i16; 7]);
+    state_machine_test!(state_machine_i16_8, [i16; 8]);
+    state_machine_test!(state_machine_i16_19, [i16; 9]);
+    state_machine_test!(state_machine_i16_10, [i16; 10]);
+    state_machine_test!(state_machine_i16_11, [i16; 11]);
+    state_machine_test!(state_machine_i16_12, [i16; 12]);
+    state_machine_test!(state_machine_i16_13, [i16; 13]);
+    state_machine_test!(state_machine_i16_14, [i16; 14]);
+    state_machine_test!(state_machine_i16_15, [i16; 15]);
+    state_machine_test!(state_machine_i16_16, [i16; 16]);
+
+    state_machine_test!(state_machine_i32_1, [i32; 1]);
+    state_machine_test!(state_machine_i32_2, [i32; 2]);
+    state_machine_test!(state_machine_i32_3, [i32; 3]);
+    state_machine_test!(state_machine_i32_4, [i32; 4]);
+    state_machine_test!(state_machine_i32_5, [i32; 5]);
+    state_machine_test!(state_machine_i32_6, [i32; 6]);
+    state_machine_test!(state_machine_i32_7, [i32; 7]);
+    state_machine_test!(state_machine_i32_8, [i32; 8]);
+
+    state_machine_test!(state_machine_i64_1, [i64; 1]);
+    state_machine_test!(state_machine_i64_2, [i64; 2]);
+    state_machine_test!(state_machine_i64_3, [i64; 3]);
+    state_machine_test!(state_machine_i64_4, [i64; 4]);
+
+    state_machine_test!(state_machine_isize_1, [isize; 1]);
+    state_machine_test!(state_machine_isize_2, [isize; 2]);
+    state_machine_test!(state_machine_isize_3, [isize; 3]);
+    state_machine_test!(state_machine_isize_4, [isize; 4]);
+
+    state_machine_test!(state_machine_i128_1, [i128; 1]);
+    state_machine_test!(state_machine_i128_2, [i128; 2]);
 }
